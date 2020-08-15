1c4cfdc99c7177a7360e6218d940943bb484cd1e
AlejandraVU <m.alejandra3003@gmail.com>
2020-08-14 23:41:49 -0500

    creacion web site portafolio

---
+++ webSite-AlejandraV/src/app/@widgets/img-ref/img-ref.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-ref',
+  templateUrl: './img-ref.component.html',
+  styleUrls: ['./img-ref.component.css']
+})
+export class ImgRefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ webSite-AlejandraV/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {AboutModule} from "./@components/about/about.module";
+import {ContactModule} from "./@components/contact/contact.module";
+import {CopyrightModule} from "./@components/copyright/copyright.module";
+import {FooterModule} from "./@components/footer/footer.module";
+import {HeaderModule} from "./@components/header/header.module";
+import {NavbarModule} from "./@components/navbar/navbar.module";
+import {PortafolioModule} from "./@components/portafolio/portafolio.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AboutModule,
+    ContactModule,
+    CopyrightModule,
+    FooterModule,
+    HeaderModule,
+    NavbarModule,
+    PortafolioModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
